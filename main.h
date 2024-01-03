812f2ab220eef502ce59899d8dc1e28d82566c9e
SuperPims <indexes-benzine0p@icloud.com>
2024-01-03 18:29:34 +0100

    Define structures and prototypes for Monty interpreter

---
+++ main.h
@@ -0,0 +1,46 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct monty_stack_s - doubly linked list representation of a stack or queue
+ * @n: integer
+ * @prev: points to the previous element of the stack or queue
+ * @next: points to the next element of the stack or queue
+ *
+ * Description: Doubly linked list node structure
+ *              for stack, queues, LIFO, FIFO
+ */
+typedef struct monty_stack_s
+{
+	int n;
+	struct monty_stack_s *prev;
+	struct monty_stack_s *next;
+} monty_stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: Opcode and its function
+ *              for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(monty_stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Prototypes */
+void push(monty_stack_t **stack, int n, unsigned int line_number);
+void pall(monty_stack_t **stack);
+
+void handle_opcode(
+	monty_stack_t **stack, char *opcode, unsigned int line_number);
+
+#endif
